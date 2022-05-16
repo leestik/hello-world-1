244ca8d62c1ef555f6e28f1e965f33a69bef126f
leestik <103378237+leestik@users.noreply.github.com>
2022-05-17 04:31:02 +0600

    Update tasks.py

--- tasks.py
+++ tasks.py
@@ -1,16 +1,18 @@
-per_cent = {'ТКБ': 5.6, 'СКБ': 5.9, 'ВТБ': 4.28, 'СБЕР': 4.0}
-money = int(input("Введите сумму вклада  "))
-
-tkb = per_cent.get('ТКБ')
-ckb = per_cent.get('СКБ')
-vtb = per_cent.get('ВТБ')
-sber = per_cent.get('СБЕР')
-
-deposit1 = tkb*money
-deposit2 = ckb*money
-deposit3 = vtb*money
-deposit4 = sber*money
-
-deposits = [deposit1, deposit2, deposit3,deposit4]
-maxdeposit = max(deposits)
-print("Максимальная сумма, которую вы можете заработать —", maxdeposit)
+print('Сколько билетов желаете приобрести?' '  ')
+ticket = int(input())
+price1 = 990
+price2 = 1390
+summa1 = 990 * ticket
+summa2 = 1390 * ticket
+print('Сколько Вам лет?' '  ')
+age = int(input())
+if age < 18:
+    print('Для вас вход на конференцию бесплатный')
+elif (18 <= age <= 25) and (ticket > 3):
+    print('Стоимость билетов  ', (summa1 - (summa1 / 100 * 10)), 'рублей')
+elif (18 <= age <= 25) and (ticket < 3):
+    print('Стоимость билетов  ', summa1, 'рублей')
+elif (age > 25) and (ticket > 3):
+    print('Стоимость билетов  ', (summa2 - (summa2 / 100 * 10)), 'рублей')
+else:
+    print('Стоимость билетов  ', summa2, 'рублей')
